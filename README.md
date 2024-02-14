f2a96db246bdf1eef3172792434b1de3862c93a8
kaushal <kaushal.kishore@smartmile.com>
2024-02-14 16:16:51 +0530

    kk

--- README.md
+++ README.md
@@ -11,3 +11,20 @@ AWSConfigRulesExecutionRole	AWS managed	Directly
 AWSLambda_FullAccess	AWS managed	Directly
 CloudWatchEventsFullAccess	AWS managed	Directly
 IAMFullAccess	AWS managed	Directly
+
+User
+what is s3_key?
+
+ChatGPT
+The s3_key refers to the key or path of the object within the S3 bucket where you want to upload the file. In Amazon S3, objects are stored in buckets, and each object has a unique key that serves as its identifier within the bucket.
+
+For example, if you want to upload the file tmpfile.txt to a bucket named my-bucket and store it in a folder named folder, the s3_key would be 'folder/tmpfile.txt'.
+
+Here's how you can use the s3_key parameter in the upload_to_s3 function:
+
+python
+Copy code
+def upload_to_s3(local_file_path, bucket_name, s3_key):
+    s3 = boto3.client('s3')
+    s3.upload_file(local_file_path, bucket_name, s3_key)
+When you call this function, you pass the local file path (local_file_path), the name of the S3 bucket (bucket_name), and the desired key (s3_key) where you want to store the file in the bucket.
--- testproj/testapp/views.py
+++ testproj/testapp/views.py
@@ -3,23 +3,57 @@ from .models import *
 from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# from django.contrib.auth.models import User
+import datetime
+import boto3
+
+def lambda_handler():
+    
+    s3_client = boto3.client('s3')
+    
+    bucket = "zappa8368420316"
+    key = "temp.txt"
+    datainput = "---------aaa--------"
+    
+    filepath = "/tmp/" +  key
+    
+    # s3_client.download_file(bucket, key, filepath)
+    
+    f = open(filepath, "a")
+    f.write("\n" + datainput)
+    f.close()
+    
+    s3_client.upload_file(filepath, bucket, key)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps("File updated successfully.")
+    }
+
 
 class Dropdown1InfoGetApi(APIView):
 
-    def get(self,request):
-        # pass
-        # d=Dropdown1.objects.filter().order_by("id")
-        # biS=Dropdown1Serializer001(d,many=True)
-        # resultMain=biS.data
-        rr=    {
-        "id": 1,
-        "name": "Val1",
-        "shortName": "V1"
-        }        
+    def get(self, request):
+
+        data_to_write = '\n'+str(datetime.datetime.now())+' - Temporary data for Dropdown1InfoGetApi.'
+        
+
+        # tmp_file_path = write_to_temp_file(data_to_write)
+
+        bucket_name = 'zappa8368420316'
+        s3_key = 'tmpfile.txt'
+        # upload_to_s3(tmp_file_path, bucket_name, s3_key)
+        # content = read_from_temp_file(tmp_file_path)
+        # print(content)
+        a= lambda_handler()
+        rr = {
+            "id": 1,
+            "name": "Val1",
+            "shortName": "V1",
+            "lamba":a
+        }
         return Response([rr], status=200)
-    
-    def post(self,request):
+
+    def post(self, request):
         pass
 
 class Dropdown2InfoGetApi(APIView):
@@ -56,3 +90,19 @@ class Dropdown3InfoGetApi(APIView):
     def post(self,request):
         pass    
 # Create your views here.
+# def write_to_temp_file(data):
+#     tmp_file_path = 'tmpfile.txt'
+#     with open(tmp_file_path, 'a') as file:
+#         file.write(data)
+#         print("lllllll", data)
+#     print(tmp_file_path)
+#     return tmp_file_path
+
+# def read_from_temp_file(file_path):
+#     with open(file_path, 'r') as file:
+#         content = file.read()
+#     return content
+
+# def upload_to_s3(local_file_path, bucket_name, s3_key):
+#     s3 = boto3.client('s3')
+#     s3.upload_file(local_file_path, bucket_name, s3_key)    
